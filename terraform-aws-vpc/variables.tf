bfe0f98910f39c20e27d2702c21023b8407e87f0
Rohit Patil <74750524+rohit1097@users.noreply.github.com>
2023-03-08 14:56:01 +0530

    Update variables.tf

--- terraform-aws-vpc/variables.tf
+++ terraform-aws-vpc/variables.tf
@@ -1,7 +1,7 @@
 variable "cidr" {
   description = "The CIDR block for the VPC. Default value is a valid CIDR, but not acceptable by AWS and should be overridden"
   type        = string
-  default     = "10.0.0.0/16"
+  default     = "10.0.0.0/1"
 }
 variable "instance_tenancy" {
   description = "A tenancy option for instances launched into the VPC"
